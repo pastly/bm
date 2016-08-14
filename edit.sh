4cf9208c523b4334c4944c9a53fa15be39e964ff
Matt Traudt <sirmatt@ksu.edu>
2016-08-13 23:18:32 -0400

    Add edit shows ambiguous files

--- edit.sh
+++ edit.sh
@@ -4,18 +4,19 @@ source internal/globals.sh
 
 set_editor
 
-SEARCH_VALUE="$1"
+SEARCH_VALUE="$@"
 SEARCH_VALUE="$(echo ${SEARCH_VALUE} | to_lower | strip_space)"
 
-NUM_FOUND_FILES=$(find "${POST_DIR}" -type f -name "${SEARCH_VALUE}*" | wc --lines)
+NUM_FOUND_FILES=$(find "${POST_DIR}" -type f -name "*${SEARCH_VALUE}*" | wc --lines)
 
 if [[ "${NUM_FOUND_FILES}" == "1" ]]
 then
-	${ED} $(find ${POST_DIR} -type f -name "${SEARCH_VALUE}*")
+	${ED} $(find ${POST_DIR} -type f -name "*${SEARCH_VALUE}*")
 	make
 elif [[ "${NUM_FOUND_FILES}" > "1" ]]
 then
 	echo "Ambiguous search term ${SEARCH_VALUE}"
+	find ${POST_DIR} -type f -name "*${SEARCH_VALUE}*"
 else
 	echo "Could not find posts matching ${SEARCH_VALUE}"
 fi
