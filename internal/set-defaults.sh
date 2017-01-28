9bebe1720c50b85afd88f7edc41bc956342bce2b
Matt Traudt <sirmatt@ksu.edu>
2017-01-28 15:41:42 -0500

    These line endings don't need '\'

--- internal/set-defaults.sh
+++ internal/set-defaults.sh
@@ -9,54 +9,54 @@
 # with or removed. It shouldn't have been, as per the big warning at the top
 # of it.
 
-[[ "${BLOG_SUBTITLE}" == "" ]] && \
+[[ "${BLOG_SUBTITLE}" == "" ]] &&
 	BLOG_SUBTITLE="Where super awesome things happen"
-[[ "${BLOG_TITLE}" == "" ]] && \
+[[ "${BLOG_TITLE}" == "" ]] &&
 	BLOG_TITLE="My Super Awesome Blog"
-[[ "${CP_SRC_FILES_TO_BUILD}" == "" ]] && \
+[[ "${CP_SRC_FILES_TO_BUILD}" == "" ]] &&
 	CP_SRC_FILES_TO_BUILD="no"
-[[ "${CREATE_HELP_VERBOSITY}" == "" ]] && \
+[[ "${CREATE_HELP_VERBOSITY}" == "" ]] &&
 	CREATE_HELP_VERBOSITY="long"
-[[ "${DATE_FRMT}" == "" ]] && \
+[[ "${DATE_FRMT}" == "" ]] &&
 	DATE_FRMT="%d %b %Y"
-[[ "${DEFAULT_AUTHOR}" == "" ]] && \
+[[ "${DEFAULT_AUTHOR}" == "" ]] &&
 	DEFAULT_AUTHOR="$(whoami)"
-[[ "${DEFAULT_INDEX_BY}" == "" ]] && \
+[[ "${DEFAULT_INDEX_BY}" == "" ]] &&
 	DEFAULT_INDEX_BY="none"
-[[ "${ED}" == "" ]] && \
+[[ "${ED}" == "" ]] &&
 	ED="${EDITOR}"
-[[ "${GPG_FINGERPRINT}" == "" ]] && \
+[[ "${GPG_FINGERPRINT}" == "" ]] &&
 	GPG_FINGERPRINT=""
-[[ "${GPG_SIGN_PAGES}" == "" ]] && \
+[[ "${GPG_SIGN_PAGES}" == "" ]] &&
 	GPG_SIGN_PAGES="no"
-[[ "${LICENSE_TEXT}" == "" ]] && \
+[[ "${LICENSE_TEXT}" == "" ]] &&
 	LICENSE_TEXT=""
-[[ "${LONG_DATE_FRMT}" == "" ]] && \
+[[ "${LONG_DATE_FRMT}" == "" ]] &&
 	LONG_DATE_FRMT="%d %b %Y at %l:%M %P"
-[[ "${MAKE_SHORT_POSTS}" == "" ]] && \
+[[ "${MAKE_SHORT_POSTS}" == "" ]] &&
 	MAKE_SHORT_POSTS="yes"
-[[ "${MONTHLY_INDEX_DATE_FRMT}" == "" ]] && \
+[[ "${MONTHLY_INDEX_DATE_FRMT}" == "" ]] &&
 	MONTHLY_INDEX_DATE_FRMT="%b %Y"
-[[ "${MULTI_MATCH_STRAT}" == "" ]] && \
+[[ "${MULTI_MATCH_STRAT}" == "" ]] &&
 	MULTI_MATCH_STRAT="simple"
-[[ "${POST_INDEX_BY}" == "" ]] && \
+[[ "${POST_INDEX_BY}" == "" ]] &&
 	POST_INDEX_BY="${DEFAULT_INDEX_BY}"
-[[ "${POSTS_ON_HOMEPAGE}" == "" ]] && \
+[[ "${POSTS_ON_HOMEPAGE}" == "" ]] &&
 	POSTS_ON_HOMEPAGE="5"
-[[ "${PREFER_SHORT_POSTS}" == "" ]] && \
+[[ "${PREFER_SHORT_POSTS}" == "" ]] &&
 	PREFER_SHORT_POSTS="no"
-[[ "${PREVIEW_MAX_WORDS}" == "" ]] && \
+[[ "${PREVIEW_MAX_WORDS}" == "" ]] &&
 	PREVIEW_MAX_WORDS="300"
-[[ "${REBUILD_POLICY}" == "" ]] && \
+[[ "${REBUILD_POLICY}" == "" ]] &&
 	REBUILD_POLICY="asap"
-[[ "${ROOT_URL}" == "" ]] && \
+[[ "${ROOT_URL}" == "" ]] &&
 	ROOT_URL=""
-[[ "${SIGNIFICANT_MOD_AFTER}" == "" ]] && \
-	SIGNIFICANT_MOD_AFTER="1" || \
+[[ "${SIGNIFICANT_MOD_AFTER}" == "" ]] &&
+	SIGNIFICANT_MOD_AFTER="1" ||
 	SIGNIFICANT_MOD_AFTER="$((${SIGNIFICANT_MOD_AFTER}))"
-[[ "${TAG_INDEX_BY}" == "" ]] && \
+[[ "${TAG_INDEX_BY}" == "" ]] &&
 	TAG_INDEX_BY="${DEFAULT_INDEX_BY}"
-[[ "${YEARLY_INDEX_DATE_FRMT}" == "" ]] && \
+[[ "${YEARLY_INDEX_DATE_FRMT}" == "" ]] &&
 	YEARLY_INDEX_DATE_FRMT="%Y"
 
 # Error checking
@@ -64,17 +64,17 @@
 # This should always end up being executed. There are certain combinations of
 # options that can be created that are invalid.
 
-[[ "${PREFER_SHORT_POSTS}" == "yes" ]] && \
-[[ "${MAKE_SHORT_POSTS}" != "yes" ]] && \
-	echo "error: PREFER_SHORT_POSTS requires MAKE_SHORT_POSTS" && \
+[[ "${PREFER_SHORT_POSTS}" == "yes" ]] &&
+[[ "${MAKE_SHORT_POSTS}" != "yes" ]] &&
+	echo "error: PREFER_SHORT_POSTS requires MAKE_SHORT_POSTS" &&
 	exit 1
 
-[[ "${GPG_FINGERPRINT}" != "" ]] && \
-! which gpg &> /dev/null && \
-	echo "error: GPG_FINGERPRINT set but gnupg doesn't seem to be installed" && \
+[[ "${GPG_FINGERPRINT}" != "" ]] &&
+! which gpg &> /dev/null &&
+	echo "error: GPG_FINGERPRINT set but gnupg doesn't seem to be installed" &&
 	exit 1
 
-[[ "${GPG_FINGERPRINT}" == "" ]] && \
-[[ "${GPG_SIGN_PAGES}" == "yes" ]] && \
-	echo "error: GPG_SIGN_PAGES enabled but GPG_FINGERPRINT needs to be set" && \
+[[ "${GPG_FINGERPRINT}" == "" ]] &&
+[[ "${GPG_SIGN_PAGES}" == "yes" ]] &&
+	echo "error: GPG_SIGN_PAGES enabled but GPG_FINGERPRINT needs to be set" &&
 	exit 1
